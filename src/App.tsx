a11e3929b5b3e986799509769f06485dcbd13d93
Julien Marescaux <marescaux.julien@gmail.com>
2026-08-19 13:24:31 +0200

    feat: add search input with state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [query, setQuery] = useState<string>("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
   return (
     <div>
       <h1>Github User Search</h1>
+      <input
+        type="text"
+        value={query}
+        onChange={handleChange}
+        placeholder="Rechercher un utilisateur Github..."
+      />
     </div>
   );
 }
